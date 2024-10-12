f4aa641812ccd06ef77d5906ab3e98a0615096f7
asmaaHamdym <asmahamdym@gmail.com>
2024-10-12 11:40:00 +0300

    "Added 3 lines to app.js to select elements and set text content"

---
+++ app.js
@@ -0,0 +1,3 @@
+const nanodegreeCard = document.querySelector(".card");
+const programTitle = document.querySelector(".card__content--h5");
+programTitle.textContent = "Everything You Need to Know About Data";
